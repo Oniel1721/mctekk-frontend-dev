059c6020b29380539edd718d2e67425a9873804b
Oniel1721 <oni1san7alv21@gmail.com>
2024-04-24 15:22:51 -0400

    feat: password confirmation validation

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -5,7 +5,11 @@ import { redirect } from "next/navigation";
 
 export const handleRegister = async (data: FormData) => {
   "use server";
-  const results = registerSchema.safeParse(formDataToObject(data));
+  const object = formDataToObject(data);
+  if (object.password !== object.password_confirmation) {
+    throw new Error("Password and password confirmation do not match");
+  }
+  const results = registerSchema.safeParse(object);
   if (!results.success) {
     throw new Error(results.error.errors[0].message);
   }
