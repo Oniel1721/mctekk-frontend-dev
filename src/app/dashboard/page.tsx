7f43fa5f750a58900bdd2f619a4de582d671b20a
Oniel1721 <oni1san7alv21@gmail.com>
2024-04-24 12:47:32 -0400

    feat: user info displayed on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { LogoutButton } from "@/components/LogoutButton";
 import { getToken } from "@/lib/cookies";
+import { getUserInfo } from "@/services/get-user-info";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
@@ -9,9 +10,13 @@ export default async function DashboardPage() {
   if (!token) {
     redirect("/login");
   }
+  const user = await getUserInfo();
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Welcome back, {user.firstname}!</p>
+      <p>Id: {user.uuid}</p>
+      <p>Email: {user.email}</p>
       <LogoutButton />
     </div>
   );
