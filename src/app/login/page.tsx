51d1a1c3f939d59b76a6ee6f42b3502ecf039de1
Oniel1721 <oni1san7alv21@gmail.com>
2024-04-24 11:35:38 -0400

    feat: login form with validations

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,33 @@
+import { login } from "@/services/login";
+import { formDataToObject } from "../utils/formDataToObject";
+import { z } from "zod";
+
+const loginPayloadSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
 export default function LoginPage() {
+  const handleLogin = async (data: FormData) => {
+    "use server";
+    const results = loginPayloadSchema.safeParse(formDataToObject(data));
+    if (!results.success) {
+      console.log(results.error.errors);
+      return;
+    }
+    const payload = results.data;
+    const auth = await login(payload);
+    console.log(auth);
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <form action={handleLogin}>
+        <input name="email" type="email" placeholder="Email" />
+        <input name="password" type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
