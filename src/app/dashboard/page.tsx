e8b7e2a2556f45667b7a6da1101dc2d0b6494a72
Oniel1721 <oni1san7alv21@gmail.com>
2024-04-24 12:41:33 -0400

    feat: dashboard route protected

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,18 @@
-export default function DashboardPage() {
+"use server";
+
+import { LogoutButton } from "@/components/LogoutButton";
+import { getToken } from "@/lib/cookies";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const token = await getToken();
+  if (!token) {
+    redirect("/login");
+  }
   return (
     <div>
       <h1>Dashboard</h1>
+      <LogoutButton />
     </div>
   );
 }
