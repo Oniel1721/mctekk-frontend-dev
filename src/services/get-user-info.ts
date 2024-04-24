28e24743b4b278d3eb35a09ef2ce33145dc8316c
Oniel1721 <oni1san7alv21@gmail.com>
2024-04-24 12:47:14 -0400

    feat: get user info service implemented

---
+++ src/services/get-user-info.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { apiClient } from "@/lib/api-client";
+
+type UserInfo = {
+  uuid: string;
+  firstname: string;
+  email: string;
+};
+
+export async function getUserInfo(): Promise<UserInfo> {
+  const { uuid, firstname, email } = await apiClient.users.getUserData();
+  return {
+    uuid,
+    firstname,
+    email,
+  };
+}
